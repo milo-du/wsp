47e7e7ea97b23661135336f4993f4d2a2f3d744f
杜铭 <milodu@foxmail.com>
2017-09-30 15:10:24 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Share from '@/components/Share'
 
 Vue.use(Router)
 
@@ -10,5 +11,9 @@ export default new Router({
 		path: '/',
 		name: 'Home',
 		component: Home
+	}, {
+		path: '/share',
+		name: 'Share',
+		component: Share
 	}]
 })
